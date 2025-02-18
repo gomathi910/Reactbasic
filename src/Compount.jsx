f50278b8235dfdee494b97185d80f1a11c7cbbb8
gomathi910 <r.gomathi96@gmail.com>
2025-02-18 17:07:11 +0530

    heading

---
+++ src/Compount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Compount() {
+  return (
+    <div>
+      <h2>this is  heading</h2>
+      <button>click</button>
+    </div>
+  )
+}
+
+export default Compount
