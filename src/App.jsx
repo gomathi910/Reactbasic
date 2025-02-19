f70c4e32986341a1ac7a3f3f75c0d95d452ca645
gomathi910 <r.gomathi96@gmail.com>
2025-02-19 17:46:05 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RefForm from './RefForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,13 +10,14 @@ function App() {
   return (
     <>
     <div>
-      <div className='card'>
+      {/* <div className='card'>
         <img src="https://i.pinimg.com/736x/d7/52/f8/d752f8585e87ca2ade9501989583cb2c.jpg" alt="kurti" />
         <h1>kurti</h1>
         <p className='price'>Rs.800</p>
         <p>cotton kurti </p>
         <button >Add To Card</button>
-      </div>
+      </div> */}
+<RefForm/>
 
     </div>
     </>
