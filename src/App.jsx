26042de2d1a22fdc51586280fe2d2432c72ac702
gomathi910 <r.gomathi96@gmail.com>
2025-02-20 10:15:43 +0530

    create a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
+    {/* <div>
       <div className='card'>
         <img src="https://i.pinimg.com/736x/d7/52/f8/d752f8585e87ca2ade9501989583cb2c.jpg" alt="kurti" />
         <h1>kurti</h1>
@@ -17,7 +18,8 @@ function App() {
         <button >Add To Card</button>
       </div>
 
-    </div>
+    </div> */}
+    <NavBar/>
     </>
   )
 }
